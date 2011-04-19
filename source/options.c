45f0d2736237862a9b014ae32f25d165d8a94d9e
pouchet <pouchet@d2347644-e8e6-0310-9099-9a715ae17836>
2011-04-19 15:57:30 +0000

    git-svn-id: https://alchemy.saclay.inria.fr/svn/users/bastoul/candl/trunk@37424 d2347644-e8e6-0310-9099-9a715ae17836

--- source/options.c
+++ source/options.c
@@ -177,7 +177,7 @@ void candl_options_help()
   "The special value 'stdin' for 'file' makes Candl to read data on\n"
   "standard input.\n\n"
   "For bug reporting or any suggestions, please send an email to the author\n"
-  "<cedric.bastoul@inria.fr> or to the maintainer of Candl:\n");
+  "<cedric.bastoul@inria.fr> or to the maintainer of Candl:\n"
   "<pouchet@cse.ohio-state.edu>.\n");
 }
 
