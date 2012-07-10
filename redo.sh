e452c858156f036728a3a00199b581621f425d85
joelpoudroux <joel.poudroux@u-psud.fr>
2012-07-10 14:06:41 +0200

    Re-add the linking with GMP in the configure Candl is now compiled by default with the GMP

--- redo.sh
+++ redo.sh
@@ -5,5 +5,8 @@ make maintainer-clean
             --with-piplib=system \
             --with-piplib-prefix=$HOME/usr \
             --with-osl=system \
-            --with-osl-prefix=$HOME/usr
+            --with-osl-prefix=$HOME/usr \
+            --with-gmp=$HOME/usr \
+            --with-gmp-library=$HOME/usr \
+            --with-gmp-include=$HOME/usr
 make -j 10
