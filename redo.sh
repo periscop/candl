a7ee8cbd4e91f7f7e25b80465143a58511b586f6
Cedric Bastoul <cedric.bastoul@u-psud.fr>
2013-02-20 15:12:01 +0100

    Remove some spurious files from the repository

--- redo.sh
+++ redo.sh
@@ -5,9 +5,6 @@ make maintainer-clean
 ./configure --prefix=$HOME/usr \
             --with-osl=system \
             --with-osl-prefix=$HOME/usr \
-            --with-gmp=$HOME/usr \
-            --with-gmp-library=$HOME/usr \
-            --with-gmp-include=$HOME/usr \
             --with-piplib=system \
             --with-piplib-prefix=$HOME/usr 
 
